1ed8a56ab364ab9fd59a51d4de123b7d08f854de
Murodali Sharipov <murodali.sharipov@pioneeringprogrammers.com>
2022-05-28 01:57:21 -0700

    AdvancedReact

---
+++ src/common/constants/index.ts
@@ -0,0 +1,9 @@
+import { api } from "./api.constants";
+
+const constants = Object.freeze(
+  Object.seal({
+    api,
+  })
+);
+
+export default constants;
---
+++ src/common/services/auth.service.ts
@@ -0,0 +1,14 @@
+import { AppDispatch } from "../../app/store";
+import { Api_Auth_Login_Body } from "../interfaces/api.interface";
+import * as i from "../interfaces/api.interface";
+
+const login = (body: Api_Auth_Login_Body):any => {
+    async (dispatch: AppDispatch) => {
+        try{
+            const {status_code, message, payload}:i. 
+
+        }catch(error:any){
+
+        }
+    }
+};
